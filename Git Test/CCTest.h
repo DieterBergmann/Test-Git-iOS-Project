1d3f2530d62f77f8cc2af8232d038a860d665d36
Dieter Bergmann <dieterbergmann@arcor.de>
2014-04-11 07:33:46 +0200

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Heike Bergmann on 11.04.14.
+//  Copyright (c) 2014 Dieter Bergmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Heike Bergmann on 11.04.14.
+//  Copyright (c) 2014 Dieter Bergmann. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
